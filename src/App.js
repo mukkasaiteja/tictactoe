7ca0075a336ec6bf01af7b51a70d88c25488237d
saiteja <87071267+mukkasaiteja@users.noreply.github.com>
2023-04-30 09:55:53 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import TicTacToe from "./components/TicTacToe";
+
+export default function App() {
+  return (
+    <div className="App">
+      <TicTacToe />
+    </div>
+  );
+}
